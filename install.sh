d6cdf91dac6112338f9284eee45309e120d9432c
julioliraup <167433857+julioliraup@users.noreply.github.com>
2024-04-22 10:47:27 -0300

    change msg of copy file

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ fi
 [ -d $DEFAULT_DIR ] && echo -ne "...\t\t Identificando arquivo padrao em $DEFAULT_DIR";
 
 mv -v lib/active_rules.sh /usr/local/pkg/suricata/active_rules.sh
-[ "$?" == "0" ] && echo -ne "[OK]\t...\t Script de alteracao: /lib/active_rules.sh";
+[ "$?" == "0" ] && echo -ne "[OK]\t...\t Script de alteracao: /usr/local/pkg/suricata/active_rules.sh";
 
 mv -v "$DEFAULT_DIR/suricata/suricata_rules.php" "backup/" && echo -ne "\t...\t backup de antiga pagina para backup/"
 curl -o "$DEFAULT_DIR/suricata/suricata_rules.php" "https://raw.githubusercontent.com/julioliraup/FreeBSD-ports/devel/security/pfSense-pkg-suricata/files/usr/local/www/suricata/suricata_rules.php" --progress-bar
