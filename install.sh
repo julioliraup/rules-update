1a3142db6d2613e8605d701a974599ad66e696a1
Julio Lira <julio.lira@GERBIT.COM.BR>
2024-04-23 07:14:54 -0300

    Change view msg and root of suricata_rules.php

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 # Versão: 1.0
 
 # Check user root
-if [ "$(id -u)" -ne 0 ]; then
+if [ "$(id -u)" -e 0 ]; then
     echo "Este script precisa ser executado como root."
     exit 1
 fi
@@ -18,17 +18,17 @@ then
 fi
 
 # Check if exits
-[ -d $DEFAULT_DIR ] && echo -ne "...\t\t Identificando arquivo padrao em $DEFAULT_DIR";
+[ -d $DEFAULT_DIR ] && echo -e "...\t\t Identificando arquivo padrao em $DEFAULT_DIR";
 
 mv -v lib/active_rules.sh /usr/local/pkg/suricata/active_rules.sh
-[ "$?" == "0" ] && echo -ne "[OK]\t...\t Script de alteracao: /usr/local/pkg/suricata/active_rules.sh";
+[ "$?" == "0" ] && echo -e "[OK]\t...\t Script de alteracao: /usr/local/pkg/suricata/active_rules.sh";
 
-mv -v "$DEFAULT_DIR/suricata/suricata_rules.php" "backup/" && echo -ne "\t...\t backup de antiga pagina para backup/"
-curl -o "$DEFAULT_DIR/suricata/suricata_rules.php" "https://raw.githubusercontent.com/julioliraup/FreeBSD-ports/devel/security/pfSense-pkg-suricata/files/usr/local/www/suricata/suricata_rules.php" --progress-bar
+mv -v "$DEFAULT_DIR/suricata/suricata_rules.php" "backup/" && echo -e "\t...\t backup de antiga pagina para backup/"
+mv -v "./suricata_rules.php" "$DEFAULT_DIR/suricata/suricata_rules.php" && echo -e "\t...\t suricata_rules.php foi substituido com sucesso"
 
 if [ "$?" == "0" ]
 then
   echo "[OK] INSTALACAO BEM SUCEDIDA"
 else
-  echo "[ER] DOWNLOAD NAO REALIZADO VOCE PODE IMPLEMENTAR MANUALMENTE.\n → https://raw.githubusercontent.com/julioliraup/FreeBSD-ports/devel/security/pfSense-pkg-suricata/files/usr/local/www/suricata/suricata_rules.php"
+  echo -e "[ER] DOWNLOAD NAO REALIZADO VOCE PODE IMPLEMENTAR MANUALMENTE.\n → https://raw.githubusercontent.com/julioliraup/FreeBSD-ports/devel/security/pfSense-pkg-suricata/files/usr/local/www/suricata/suricata_rules.php"
 fi
