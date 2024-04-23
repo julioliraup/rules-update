05b795551a5053c5d32489f62478d623a605022a
Julio Lira <julio.lira@GERBIT.COM.BR>
2024-04-23 07:16:23 -0300

    Viewer format fix

--- uninstall.sh
+++ uninstall.sh
@@ -5,7 +5,7 @@
 # Versão: 1.0
 # Check user root
 #
-if [ "$(id -u)" -ne 0 ]; then
+if [ "$(id -u)" -e 0 ]; then
     echo "Este script precisa ser executado como root."
     exit 1
 fi
@@ -18,16 +18,16 @@ then
 fi
 
 # Check if exits
-[ -d $DEFAULT_DIR ] && echo -ne "...\t\t Identificando arquivo padrao em $DEFAULT_DIR";
+[ -d $DEFAULT_DIR ] && echo -e "...\t\t Identificando arquivo padrao em $DEFAULT_DIR";
 
 rm -rfv /usr/local/pkg/suricata/active_rules.sh
-[ "$?" == "0" ] && echo -ne "[OK]\t...\t Deletado script /usr/local/pkg/suricata/active_rules.sh";
+[ "$?" == "0" ] && echo -e "[OK]\t...\t Deletado script /usr/local/pkg/suricata/active_rules.sh";
 
-mv -v "backup/suricata_rules.php" "$DEFAULT_DIR/suricata/suricata_rules.php" && echo -ne "\t...\t Restaurando arquivo php ao original"
+mv -v "backup/suricata_rules.php" "$DEFAULT_DIR/suricata/suricata_rules.php" && echo -e "\t...\t Restaurando arquivo php ao original"
 
 if [ "$?" == "0" ]
 then
   echo "[OK] REMOCAO BEM SUCEDIDA"
 else
-  echo -ne "[ER] BACKUP NAO ENCONTRADO. VERIFIQUE A VERSAO DO SEU SURICATA E BAIXE DIRETAMENTE\n → https://raw.githubusercontent.com/pfsense/FreeBSD-ports/devel/security/pfSense-pkg-suricata/files/usr/local/www/suricata/suricata_rules.php"
+  echo -e "[ER] BACKUP NAO ENCONTRADO. VERIFIQUE A VERSAO DO SEU SURICATA E BAIXE DIRETAMENTE\n → https://raw.githubusercontent.com/pfsense/FreeBSD-ports/devel/security/pfSense-pkg-suricata/files/usr/local/www/suricata/suricata_rules.php"
 fi
