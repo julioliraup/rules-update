3827d7c884366f60da68f04cc974b6b2d02bb565
jul10l1r4 <jul10l1r4@disroot.org>
2024-05-16 16:45:45 -0300

    :tada: filter for get all alerts SIDs and all policy/info rules

---
+++ filter_informational.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# script para verificar falso positivo na rede 
+# Ele listará todos os IDs que contém são da
+# categoria de INFO ou POLICY
+logfile=""
+
+if [ "$1" == "" ];
+then
+  logfile="/usr/local/share/suricata/rules/*"
+else
+  logfile="$1"
+fi
+
+grep -E 'former_category (INFO|POLICY)' $logfile | grep -o 'sid:[0-9]*;' | sed 's/sid:/1:/;s/;//'
